4fcd36e3c7a47cf75b2c2fa1ce10b6695664a9a4
shubham kumar <devshubhamyadav@gmail.com>
2022-04-01 21:02:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PdfReader from "./PdfReader";
 import pdfFile from "./ReactJS Notes.pdf";
+import "./App.css";
 const App = () => {
   return (
    <div>
-      <h1 style={{ }}  >Adobe react pdf viewer(Adobe PDF Embed API)</h1>
+      <h1 className="heading"  >Adobe react pdf viewer(Adobe PDF Embed API)</h1>
       <a
         target="_blank"
         href="https://theshubham.me/displaying-pdf-in-react-js-using-adobe-pdf-embed-api"
       >
         Read More
       </a>
-      <PdfReader style={{height:"screen",
-  width:"100%"
-  overflow:"hidden" }} url={pdfFile} />
+      <PdfReader className="pdfReader" url={pdfFile} />
       
     </div>
   )
