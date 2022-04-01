febcefd1c7df6db5c91493759e52b7f82e078907
shubham kumar <devshubhamyadav@gmail.com>
2022-04-01 21:15:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
    <div>
       <h1 className="heading"  >Adobe react pdf viewer(Adobe PDF Embed API)</h1>
-      <a
+      <a className="link"
         target="_blank"
         href="https://theshubham.me/displaying-pdf-in-react-js-using-adobe-pdf-embed-api"
       >
