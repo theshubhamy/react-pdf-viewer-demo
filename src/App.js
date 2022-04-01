08753a70f765c743c189f50cfc9f5b3fe5dcccea
shubham kumar <devshubhamyadav@gmail.com>
2022-04-01 20:10:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import pdfFile from "./ReactJS Notes.pdf";
 const App = () => {
   return (
     <div><PdfReader url={pdfFile}/>
+<a href="https://theshubham.me/displaying-pdf-in-react-js-using-adobe-pdf-embed-api">Read More<a/>
     </div>
   )
 }
