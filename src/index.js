02881ca7841705b8aebed371066eb739692b5760
shubham kumar <devshubhamyadav@gmail.com>
2022-04-01 20:00:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App url="https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea%20Brochure.pdf" />
+    <App url="ReactJS Notes.pdf" />
   </StrictMode>,
   rootElement
 );
