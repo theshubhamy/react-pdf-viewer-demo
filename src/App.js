2e9d0e19fdcdfafbb63eb5d799951236d1ba590b
shubham kumar <devshubhamyadav@gmail.com>
2022-04-01 20:06:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+import PdfReader from "./PdfReader";
 const App = () => {
   return (
-    <div><
+    <div><PdfReader url="/.pdf"/>
     </div>
   )
 }
