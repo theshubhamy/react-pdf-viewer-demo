585391ce6d704072254116cdf10394b14432c84c
shubham kumar <devshubhamyadav@gmail.com>
2022-04-01 20:05:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React from "react";
-import ViewSDKClient from "./ViewSDKClient";
-const RenderMenu = ({ url }) => {
-  const loadPDF = () => {
-    const viewSDKClient = new ViewSDKClient();
-    viewSDKClient.ready().then(() => {
-      viewSDKClient.previewFile(
-        "pdf-div",
-        {
-          defaultViewMode: "FIT_WIDTH",
-          showAnnotationTools: false,
-          showLeftHandPanel: false,
-          showPageControls: false,
-          showDownloadPDF: false,
-          showPrintPDF: false,
-        },
-        url
-      );
-    });
-  };
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="mt-28">
-      <div
-        id="pdf-div"
-        className="full-window-div border border-gray-100 h-screen"
-        onDocumentLoad={loadPDF()}
-      ></div>
+    <div><
     </div>
-  );
-};
-export default RenderMenu;
+  )
+}
+
+export default App
